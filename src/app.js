14f2ecd7e0a071ce2ae6993be374eba9a38310d5
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-04-05 01:41:52 +0100

    App setting json and integration testing in utilising the ItemCollection class in the app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
+import ItemCollection from './components/item-collection/item-collection';
+import * as settings from './appSetting.json'
 // source goes here
-const app = function() {
-  console.log('hello from app!');
+export const appSettings = settings.default || {};
+
+const app = () => {
+  const itemCollection = new ItemCollection(appSettings)
+  document.body.appendChild(itemCollection.element);
 };
 
 export default app;
--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,56 @@
-import app from '../src/app';
+import app, {appSettings} from '../src/app';
+
+beforeEach(() => {
+  document.body.innerHTML = '';
+});
 
 describe('app', function() {
   it('does nothing', function() {
     expect(true).toBe(true);
   });
+
+  it('should render the item-collection component and children', () => {
+    app();
+    const itemCollection = document.querySelectorAll('.item__collection');
+    expect(itemCollection.length).toEqual(1);
+    expect(itemCollection[0].children.length).toEqual(appSettings.to);
+  });
+
+  it('should render the item-button components appSettings.to times', () => {
+    app();
+    const itemButtons = document.querySelectorAll('.item__button');
+    expect(itemButtons.length).toEqual(appSettings.to);
+  });
+});
+
+describe('integration test', () => {
+  it('should work correctly on integration', () => {
+    app();
+    const itemButtons = document.querySelectorAll('.item__button');
+    expect(itemButtons.length).toEqual(appSettings.to);
+
+    let itemButtonsHighlighted = document.querySelectorAll('.highlight');
+    expect(itemButtonsHighlighted.length).toBe(0);
+
+    // click #72
+    let number = 72;
+    let button = itemButtons[number - 1];
+    button.dispatchEvent(new MouseEvent('click'));
+    itemButtonsHighlighted = document.querySelectorAll('.highlight');
+    expect(itemButtonsHighlighted.length).toBe(2);
+    expect(itemButtonsHighlighted[0].innerHTML).toBe("72");
+    expect(itemButtonsHighlighted[1].innerHTML).toBe("144");
+
+    // click #10
+    number = 10;
+    button = itemButtons[number - 1];
+    button.dispatchEvent(new MouseEvent('click'));
+    itemButtonsHighlighted = document.querySelectorAll('.highlight');
+    expect(itemButtonsHighlighted.length).toBe(14);
+
+    // click #10 again   
+    button.dispatchEvent(new MouseEvent('click'));
+    itemButtonsHighlighted = document.querySelectorAll('.highlight');
+    expect(itemButtonsHighlighted.length).toBe(0);
+  })
 });
